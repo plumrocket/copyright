00ea025838133a35ada76977b40917d406fd880e
plumrocket <devteam@plumrocket.com>
2021-03-22 10:29:04 +0200

    Create Copyright.php

---
+++ Copyright.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @package     Plumrocket
+ * @copyright   Copyright (c) 2021 Plumrocket Inc. (https://www.plumrocket.com)
+ * @license     https://www.plumrocket.com/license/  End-user License Agreement
+ */
